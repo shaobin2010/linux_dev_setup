287b751462e39bdf589ec4b47d16dca20f5e55aa
shawn wang <shaobin2010@163.com>
2020-06-28 09:29:31 -0400

    update ubuntu apt source

--- sys_scripts/change-apt-source-list-to-163.sh
+++ sys_scripts/change-apt-source-list-to-163.sh
@@ -4,18 +4,16 @@ set -e
 
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
-echo 'deb http://mirrors.163.com/ubuntu/ precise-updates main restricted' > /etc/apt/sources.list 
-echo 'deb-src http://mirrors.163.com/ubuntu/ precise-updates main restricted' >> /etc/apt/sources.list
-echo 'deb http://mirrors.163.com/ubuntu/ precise universe' >> /etc/apt/sources.list
-echo 'deb-src http://mirrors.163.com/ubuntu/ precise universe' >> /etc/apt/sources.list
-echo 'deb http://mirrors.163.com/ubuntu/ precise-updates universe' >> /etc/apt/sources.list
-echo 'deb-src http://mirrors.163.com/ubuntu/ precise-updates universe' >> /etc/apt/sources.list
-echo 'deb http://mirrors.163.com/ubuntu/ precise multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://mirrors.163.com/ubuntu/ precise multiverse' >> /etc/apt/sources.list
-echo 'deb http://mirrors.163.com/ubuntu/ precise-updates multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://mirrors.163.com/ubuntu/ precise-updates multiverse' >> /etc/apt/sources.list
-echo 'deb http://mirrors.163.com/ubuntu/ precise-backports main restricted universe multiverse' >> /etc/apt/sources.list
-echo 'deb-src http://mirrors.163.com/ubuntu/ precise-backports main restricted universe multiverse' >> /etc/apt/sources.list
+deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse > /etc/apt/sources.list 
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse  >> /etc/apt/sources.list
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse  >> /etc/apt/sources.list
+deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse  >> /etc/apt/sources.list
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse  >> /etc/apt/sources.list
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse  >> /etc/apt/sources.list
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse  >> /etc/apt/sources.list
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse  >> /etc/apt/sources.list
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse  >> /etc/apt/sources.list
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse  >> /etc/apt/sources.list
 
 apt-get update
 
